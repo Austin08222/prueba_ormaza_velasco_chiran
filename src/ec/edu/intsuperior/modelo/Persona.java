5a9f8cec3218181236297387e3e54934cea1be59
jonat <jonat@LAPTOP-K90FNRME>
2022-07-12 03:35:47 -0500

    esta es la version de jormaza

--- src/ec/edu/intsuperior/modelo/Persona.java
+++ src/ec/edu/intsuperior/modelo/Persona.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ec.edu.intsuperior.modelo;
 
-/**
- *
- * @author jonat
- */
+package ec.edu.intsuperior.modelo;
+import java.util.Date;
+
 public class Persona {
+ 
+    
     
 }
