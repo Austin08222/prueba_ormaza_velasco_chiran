f76d989f96c884c58512aad4bd62578b53d6258f
ANDY7u7 <108316841+ANDY7u7@users.noreply.github.com>
2022-07-12 11:49:48 -0500

    Vercion 2 de achiran

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -5,10 +5,40 @@
  */
 package ec.edu.intsuperior.vista;
 
+import javax.swing.JOptionPane;
+import ec.edu.intsuperior.modelo.Persona; 
+import java.time.Instant;
+import java.util.Date;
+import java.util.Scanner;
 /**
  *
  * @author jonat
  */
 public class Aplicacion {
-    
+      public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
+        Persona p1= new Persona();
+         System.out.println("Ingese tu cedula");
+        p1.setCi(leer.next());
+        System.out.println("Ingresa tu primer nombre");
+        p1.setApellido1(leer.next());
+        System.out.println("Ingresa tu segundo nombre");
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa tu segundo nombre"));
+        System.out.println("Ingesa tu primer apellido");
+        p1.setApellido1(JOptionPane.showInputDialog("Ingesa tu primer apellido"));
+        System.out.println("Ingresa tu segundo apellido");
+        p1.setApellidos2(leer.next());
+        System.out.println("Ingresa tu direccion");
+        p1.setDireccion(leer.next());
+        System.out.println("Ingresa tu numero telefonico");
+        p1.setCelular(leer.next());
+        System.out.println("Ingresa tu correo electronico");
+        p1.setCorreo(leer.next());
+        System.out.println("Ingresa tu fecha de nacimeinto");
+        
+        
+        
+        JOptionPane.showMessageDialog(null,p1.toString());
+        
+    }
 }
