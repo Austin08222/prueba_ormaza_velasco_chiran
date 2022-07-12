2a786ebba4c674d527e7193d521543bb639f8930
jonat <jonat@LAPTOP-K90FNRME>
2022-07-12 04:25:08 -0500

    esta es la version de jormaza v2

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -1,11 +1,22 @@
 
 package ec.edu.intsuperior.vista;
 
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "este es el repositorio de jormaza");
+        Scanner leer= new Scanner(System.in);
+        Persona p1=new Persona();
+        System.out.println("Ingresa la cedula :");
+         p1.setCi(leer.next());
+        System.out.println(" Ingresa nombres1: ");
+        p1.setNombre1(leer.next());
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa tu segundo nombre: "));
+        p1.setApellido1(JOptionPane.showInputDialog("Ingresa tu apellido paterno: "));
+        
+        JOptionPane.showMessageDialog(null,p1.toString());
     }
     
 }
